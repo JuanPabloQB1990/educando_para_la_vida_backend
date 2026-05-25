95939848572bccd92f36d9e127a6beddbcfed25a
juan pablo <juanpabloqb1990@gmail.com>
2026-05-25 17:21:11 -0500

    falta emails sin spam

--- src/config/awsSES.ts
+++ src/config/awsSES.ts
@@ -1,4 +1,6 @@
 import { SESClient } from "@aws-sdk/client-ses";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const sesClient = new SESClient({
   region: process.env.AWS_REGION,
--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,12 +1,14 @@
-import {
-  SendEmailCommand,
-} from "@aws-sdk/client-ses";
+import { SendEmailCommand } from "@aws-sdk/client-ses";
+import dotenv from "dotenv";
+dotenv.config();
 
 import { sesClient } from "../config/awsSES";
 
 export class EmailService {
   static async sendMail(to: string, subject: string, html: string) {
     try {
+      console.log(process.env.SES_FROM_EMAIL);
+      
       const command = new SendEmailCommand({
         Source: process.env.SES_FROM_EMAIL!,
         Destination: {
